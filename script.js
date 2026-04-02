0863ddf8894c309d47016bdfb684b4376cb98e2d
mateusrmelo-collab <mateus.r.melo@edu.senai.br>
2026-04-02 18:54:53 -0300

    atualização definitiva

---
+++ script.js
@@ -0,0 +1,37 @@
+let cliques = 0;
+
+const botao = document.getElementById("botaoDesistir");
+const mensagem = document.getElementById("mensagem");
+const musica = document.getElementById("musica");
+
+botao.addEventListener("click", function() {
+    cliques++;
+
+    if (cliques === 1) {
+        mensagem.textContent = "Você já está desistindo? Que decepção.";
+    } 
+    else if (cliques === 2) {
+        mensagem.textContent = "Nem tentou direito.";
+    } 
+    else if (cliques === 3) {
+        mensagem.textContent = "Isso não vai te salvar.";
+    } 
+    else if (cliques === 4) {
+        mensagem.textContent = "Clique mais uma vez.";
+    } 
+    else if (cliques === 5) {
+        mensagem.textContent = "Você continua falhando.";
+    } 
+    else if (cliques === 6) {
+        mensagem.textContent = "Última chance...";
+    } 
+    else if (cliques === 7) {
+        mensagem.textContent = "Você não pode sair!";
+        botao.textContent = "SEM ESCAPATÓRIA";
+
+        musica.currentTime = 6;
+        musica.play();
+
+        document.body.classList.add("modo-troll");
+    }
+});
